01fca1116c6967dbf6730443cedce0bb216780ba
Fellipe Juncal <fjuncal7@gmail.com>
2025-01-11 20:25:26 -0300

    Finalizando a implementação do css do componente de usuario

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,6 +1,7 @@
 import { MdLocationPin } from "react-icons/md";
 import { UserProps } from "../types/user";
 import { Link } from "react-router-dom";
+import classes from "./User.module.css";
 
 export default function User({
   login,
@@ -10,24 +11,24 @@ export default function User({
   location,
 }: UserProps) {
   return (
-    <div>
+    <div className={classes.user}>
       <img src={avatar_url} alt={login} />
       <h2>{login}</h2>
       {location && (
-        <p>
+        <p className={classes.location}>
           <MdLocationPin />
           <span>{location}</span>
         </p>
       )}
 
-      <div>
+      <div className={classes.stats}>
         <div>
           <p>Seguidores:</p>
-          <p>{followers}</p>
+          <p className={classes.number}>{followers}</p>
         </div>
         <div>
           <p>Seguindo:</p>
-          <p>{following}</p>
+          <p className={classes.number}>{following}</p>
         </div>
       </div>
       <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
