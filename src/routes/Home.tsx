5488fb71456971e003ec248cc7fabd3692d39da7
Fellipe Juncal <fjuncal7@gmail.com>
2025-01-11 20:12:59 -0300

    Criando componente de erro e estado de erro, renderizar apenas se ele tiver true

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import Search from "../components/Search";
 import { UserProps } from "../types/user";
 import User from "../components/User";
+import Erro from "../components/Erro";
 
 export default function Home() {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loadUser = async (userName: string) => {
     const response = await fetch(`https://api.github.com/users/${userName}`);
     const data = await response.json();
+    if (response.status === 404) {
+      setError(true);
+      return;
+    }
     // const [avatar_url, login, name, location, followers, following] = data;
     // const userData: UserProps = {
     //   avatar_url,
@@ -26,6 +32,7 @@ export default function Home() {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Erro />}
     </div>
   );
 }
